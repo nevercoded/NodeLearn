75ed7380893b293af0e058c2bd4337c3811b1fc0
nevercoded <nevercoded@outlook.com>
2013-03-02 15:30:37 -0800

    Added express module and created a logger module. This is just to see how both modules can be installed

--- sunodetest/app.js
+++ sunodetest/app.js
@@ -1,5 +1,9 @@
 var http = require('http');
+var logger = require('./modules/logger.js');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello Node from SU..');
+  logger.trace('createServer','Sent response');
 }).listen(process.env.VMC_APP_PORT || 1337, null);
+//.listen(8003, "127.0.0.1");
