aaead3e753c26840d07ef0570ace6d3f55208c59
nevercoded <nevercoded@outlook.com>
2013-03-02 16:06:34 -0800

    added request end listener

--- sunodetest/app.js
+++ sunodetest/app.js
@@ -1,9 +1,26 @@
 var http = require('http');
+var url = require('url');
 var logger = require('./modules/logger.js');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello Node from SU..');
-  logger.trace('createServer','Sent response');
+http.createServer(function (request, response) {
+
+	request.on('end',function(){
+		// Parse the request for arguments and store them in _get variable.
+		// This function parses the url from request and returns object representation.
+		var _get = url.parse(request.url, true).query;
+		// Write headers to the response.
+		response.writeHead(200, {
+		 'Content-Type': 'text/plain'
+		});
+		// Send data and end response.
+		if(_get['data'] != undefined){
+			//response.end(request.host+" "+request.connection);
+			response.end('Welcome to node.js on: ' + _get['data']);
+		}else{
+			response.end('Welcome to node.js on: ' + new Date());
+		}
+	});	
+	logger.trace('createServer','Sent response');
+  
 }).listen(process.env.VMC_APP_PORT || 1337, null);
 //.listen(8003, "127.0.0.1");
